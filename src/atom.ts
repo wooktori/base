524e15fa1f0de2cb2d77083a00580b544ef97b0e
wooktori <jjun9503@gmail.com>
2025-03-11 14:21:35 +0900

    카테고리 기능 추가

--- src/atom.ts
+++ src/atom.ts
@@ -1,12 +1,26 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface ITodo {
-  text: string;
-  id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+    text: string;
+    id: number;
+    category: "TO_DO" | "DOING" | "DONE";
 }
 
 export const todoAtom = atom<ITodo[]>({
-  key: "todo",
-  default: [],
+    key: "todo",
+    default: [],
+});
+
+export const categoryAtom = atom({
+    key: "category",
+    default: "TO_DO",
+});
+
+export const todoSelector = selector({
+    key: "todoSelector",
+    get: ({ get }) => {
+        const todos = get(todoAtom);
+        const category = get(categoryAtom);
+        return todos.filter((todo) => todo.category === category);
+    },
 });
--- src/compoents/TodoList.tsx
+++ src/compoents/TodoList.tsx
@@ -1,20 +1,31 @@
-import { useRecoilValue } from "recoil";
-import { todoAtom } from "../atom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryAtom, todoSelector } from "../atom";
 import AddTodo from "./AddTodo";
 import Todo from "./Todo";
+import { FormEvent } from "react";
 
 export default function TodoList() {
-  const todoList = useRecoilValue(todoAtom);
-
-  return (
-    <div>
-      <h1>TodoList</h1>
-      <AddTodo />
-      <ul>
-        {todoList.map((todo) => (
-          <Todo key={todo.id} {...todo} />
-        ))}
-      </ul>
-    </div>
-  );
+    const [category, setCategory] = useRecoilState(categoryAtom);
+    const todoList = useRecoilValue(todoSelector);
+    console.log(todoList);
+    const handleChange = (e: FormEvent<HTMLSelectElement>) => {
+        setCategory(e.currentTarget.value);
+    };
+    return (
+        <div>
+            <h1>TodoList</h1>
+            <select value={category} onChange={handleChange}>
+                <option value="TO_DO">TO DO</option>
+                <option value="DOING">DOING</option>
+                <option value="DONE">DONE</option>
+            </select>
+            <AddTodo />
+            <ul>
+                {todoList?.map((todo) => (
+                    <Todo key={todo.id} {...todo} />
+                ))}
+            </ul>
+            <hr />
+        </div>
+    );
 }
